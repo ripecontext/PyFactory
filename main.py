33e6c234f35f9fdfa97f55a7e4518747192c5d4b
ripecontext <42746151+ripecontext@users.noreply.github.com>
2025-11-08 13:28:18 +0000

    draw a big ass rectangle

--- main.py
+++ main.py
@@ -9,3 +9,9 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+    window.fill((0,0,0))
+
+    pygame.draw.rect(window, (255,255,255), (40,40,500,500))
+
+    pygame.display.flip()
